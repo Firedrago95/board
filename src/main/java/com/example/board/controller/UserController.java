9dd8659e03c33d995c0641e9807aa9364acd9f9a
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-08 13:20:15 +0900

    회원가입 처리 메서드 작성

--- src/main/java/com/example/board/controller/UserController.java
+++ src/main/java/com/example/board/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -9,5 +11,22 @@ public class UserController {
     @GetMapping("/userRegForm")
     public String userRegForm() {
         return "userRegForm";
+    }
+
+    @PostMapping("/userReg")
+    public String userReg(
+        @RequestParam("name") String name,
+        @RequestParam("email") String email,
+        @RequestParam("password") String password
+    ) {
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+        System.out.println("password = " + password);
+        return "redirect:/welcome"; // 브라우저에게 자동으로 http://localhost:8080/welcome 으로 이동
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
