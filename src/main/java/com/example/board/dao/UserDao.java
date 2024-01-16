0362ada30df664a15099918ee3d177f3c2738de2
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-16 10:15:05 +0900

    Dao 추가 db와 연결하는 역할

---
+++ src/main/java/com/example/board/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.example.board.dao;
+
+import com.example.board.dto.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+// 스프링이 관리하는 Bean
+@Repository
+public class UserDao {
+    // Spring JDBC를 이용한 코드
+    @Transactional
+    public User addUser(String email, String name, String password) {
+        // Service에서 이미 트랜잭션이 시작했기 때문에, 그 트랜잭션에 포함된다.
+        // insert into user (email, name, password, regdate) values (?, ?, ?, now()); #user_id auto gen
+        // SELECT last_insert_id();
+        return null;
+    }
+
+    @Transactional
+    public void mappingUserRole(int userId) {
+        // insert into user_role(user_id, role_id) values(?, 1);
+    }
+}
+ /*
+        insert into user (email, name, password, regdate) values (?, ?, ?, now()); #user_id auto gen
+        SELECT last_insert_id();
+        insert into user_role(user_id, role_id) values(?, 1);
+ */
